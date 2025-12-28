2a1e4418ddbd0288ca72689e76a0d81da50dc646
june5815 <dev.junejune@gmail.com>
2025-12-29 01:23:49 +0900

    fix: Add .js extension to constants import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import http from "http";
-import { PORT, PUBLIC_PATH, STATIC_PATH } from "./common/config/constants";
+import { PORT, PUBLIC_PATH, STATIC_PATH } from "./common/config/constants.js";
 import { initializeSocket } from "./common/lib/socket.manager";
 import articlesRouterCA from "./inbound/routers/articles.router";
 import userRouterCA from "./inbound/routers/user.router";
